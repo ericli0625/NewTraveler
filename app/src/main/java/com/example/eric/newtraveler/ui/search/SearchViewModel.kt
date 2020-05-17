ef1f47cc89d01e5acba00a0da2824fc075f7cc3d
erichcli <eric.li@kkday.com>
2020-05-17 15:47:18 +0800

    Get multi key in the string when fetch() is called.

--- app/src/main/java/com/example/eric/newtraveler/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/eric/newtraveler/ui/search/SearchViewModel.kt
@@ -5,11 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import com.example.eric.newtraveler.network.response.AttractionDetail
 import com.example.eric.newtraveler.ui.base.BaseViewModel
 import com.kkday.scm.util.wrapper.Event
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
 class SearchViewModel(private val repository: SearchRepository) : BaseViewModel(repository) {
+
+    private val attractionList: MutableList<AttractionDetail> = mutableListOf()
 
     private val disposables = CompositeDisposable()
 
@@ -24,13 +27,18 @@ class SearchViewModel(private val repository: SearchRepository) : BaseViewModel(
         repository.fetchAndActivateRemoteConfig(::getTemplateKey)
     }
 
-    private fun getTemplateKey(key: String) {
+    private fun getTemplateKey(queries: List<String>) {
         disposables.add(
-                repository.getKeywordSearchSpotDetail(key)
+                Observable.just(queries)
+                        .flatMap { Observable.fromIterable(it) }
+                        .flatMap { repository.getKeywordSearchSpotDetail(it) }
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
+                        .doOnComplete {
+                            _showAttractionListEvent.value = Event(attractionList)
+                        }
                         .subscribe {
-                            _showAttractionListEvent.value = Event(it)
+                            attractionList += it
                         }
         )
     }
